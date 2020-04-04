563f23f1bb5c0c2336eac087ffef06f0d2a360de
atmnk <21354507+atmnk@users.noreply.github.com>
2020-04-04 15:52:56 +0530

    chore: upgrade hwrust formula to v0.0.19

--- Formula/hwrust.rb
+++ Formula/hwrust.rb
@@ -4,9 +4,9 @@
 class Hwrust < Formula
   desc "Hello World With Github Actions"
   homepage "https://github.com/atmnk/hwrust"
-  url "https://github.com/atmnk/hwrust/releases/download/v0.0.14/hwrust.tar.gz"
-  sha256 "14bcaf694a0aa3542ca428f12488e0f63175e8d575dec4e42a2b5d174b421f93"
-  version "0.0.16"
+  url "https://github.com/atmnk/hwrust/releases/download/v0.0.19/hwrust.tar.gz"
+  sha256 "f1ae19a3309980a77cea2aefeca7e459d395b1b8cf970b278c121de54fa7dbab"
+  version "0.0.19"
 
   def install
     bin.install "hwrust"
